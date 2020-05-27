412f12e334113a56a74f9912082383a11ca9d719
@itsAj <ajaymati1000@gmail.com>
2020-05-27 14:19:52 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Main from './Components/MainComponent';
-import Facebook from './Components/Facebook';
 export default class APP extends Component {
   render(){
     return(
